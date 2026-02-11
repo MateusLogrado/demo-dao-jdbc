3a4802d1de2ad9bedb11b33c200f82927d103ff1
Mateus Logrado <mateusqueirozlogrado@gmail.com>
2026-02-10 21:17:10 -0300

    insert implementation

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -24,5 +25,9 @@ public class Main {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new id = " + newSeller.getId());
 	}
 }
